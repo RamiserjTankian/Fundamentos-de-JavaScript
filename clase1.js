ba5055d643af55aa9a0eb357ba735ebaa8f0c8b3
Ramiro Diaz <milinchi_@hotmail.com>
2019-08-05 20:00:57 -0500

    Clase 1
    Clase 1, uso de variables.

---
+++ clase1.js
@@ -0,0 +1,6 @@
+var nombre = 'Ramiro', apellido = 'Diaz'
+var edad = 28
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad + ' años')
+edad = '28 años'
+console.log('Tengo '+ edad)
