fc0f88fd7e51216321990123be52033b6813871d
Ramiro Diaz <milinchi_@hotmail.com>
2019-08-05 20:36:06 -0500

    Clase 3
    Uso de enteros

---
+++ clase3.js
@@ -0,0 +1,30 @@
+var edad = 22
+// edad = edad + 1
+edad +=1
+
+var peso = 75
+
+//peso = peso - 2
+peso -= 2
+
+var sandwich = 1
+
+peso = peso + sandwich
+
+var jugarAlFutbol = 3
+
+//peso = peso - jugarAlFutbol
+
+peso -= jugarAlFutbol
+
+var precioDeVino = 200.3
+
+//var total = precioDeVino * 100 * 3 / 100
+var total = Math.round(precioDeVino * 100 * 3) / 100
+
+var totalStr = total.toFixed(3)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var persona = 2
+var cantidadDePorcionesPorPersonas = pizza/persona
