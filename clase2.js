9075591ea48018e264212483822040585d16620d
Ramiro Diaz <milinchi_@hotmail.com>
2019-08-05 20:09:24 -0500

    Clase 2
    Manipular strings en javascript.

---
+++ clase2.js
@@ -0,0 +1,11 @@
+var nombre = 'Ramiro', apellido = 'Diaz'
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLocaleLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = nombre + ' ' + apellido
+var nombreCompleto1 = `${nombre} ${apellido.toLocaleUpperCase()}`
+var str = nombre.substr(1, 2)
