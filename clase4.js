731133d38b933e82d153f046993ef2d5f9cf1579
Ramiro Diaz <milinchi_@hotmail.com>
2019-08-05 20:42:45 -0500

    Clase 4
    Uso basico de funciones

---
+++ clase4.js
@@ -0,0 +1,8 @@
+var nombre = 'Sacha', edad = 28
+function imprmirEdad(n,e) {
+    console.log(`${n} tiene ${e} años`)
+}
+imprmirEdad('Ramiro',edad)
+imprmirEdad('Jose',23)
+imprmirEdad('Toño',19)
+imprmirEdad('Elon',edad)
